cb442357e2e38638b001efc6f8716376b040908d
Walter Simoncini <walterdev@icloud.com>
2018-11-08 11:21:19 +0100

    Created some basic utility functions

---
+++ word_generator.py
@@ -0,0 +1,33 @@
+import random
+import operator
+
+def generate_word (length):
+    result = ""
+
+    for i in range(0, len(word_to_match)):
+        letter = chr(97 + int(26 * random.random()))
+        result += letter
+    
+    return result
+
+
+def fitness (word, word_to_match):
+    if (len(word) != len(word_to_match)):
+        print("Words have different lengths")
+        return
+    else:
+        score = 0
+
+        for i in range(0, len(word_to_match)):
+            if (word[i] == word_to_match[i]):
+                score += 1
+
+        return score * 100 / len(word_to_match)
+
+def compute_population_fitness(population, word_to_match):
+    fitnesses = {}
+
+    for individual in population:
+        fitnesses[individual] = fitness(word_to_match, individual)
+
+    return sorted(population.items(), key = operator.itemgetter(1), reverse = True)
